05344c80ce6c012630ac8c8663e4a9a2eaf5f15e
Rabin <rabin.awale09@gmail.com>
2025-11-12 21:11:55 +0545

    Add relationships for Task and User models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function status() {
+        return $this->belongsTo(TaskStatus::class);
+    }
+
+    public function assignees() {
+        return $this->belongsToMany(User::class, 'task_assignees');
+    }
 }
