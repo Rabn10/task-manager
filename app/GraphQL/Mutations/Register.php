43d225a1ac803294db372e22c727147d48180be4
Rabin <rabin.awale09@gmail.com>
2025-11-17 15:16:12 +0545

    Implement user registration and login mutations with Sanctum authentication

--- app/GraphQL/Mutations/Register.php
+++ app/GraphQL/Mutations/Register.php
@@ -6,7 +6,8 @@ use Nuwave\Lighthouse\Execution\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 //use Joselfonseca\LighthouseGraphQLPassport\GraphQL\Mutations\BaseAuthResolver;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 class Register
 {
     public function create($root, array $args): User
@@ -14,9 +15,33 @@ class Register
         $user = User::create([
             'name' => $args['name'],
             'email' => $args['email'],
-            'password' => $args['password'],
+            'password' => Hash::make($args['password']),
         ]);
 
         return $user;
+    }
+
+    public function login($root, array $args)
+    {
+        //Find user by email
+        $user = User::where('email',$args['email'])->first();
+       // dd($user);
+
+        if(! $user || ! Hash::check($args['password'], $user->password)) {
+            return [
+                'status' => 0,
+                'message' => 'Invalid credentials'
+            ];
+        }
+
+        //Create token (Sanctum)
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return [
+            'status' => 1,
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user
+        ];
     }
 }
