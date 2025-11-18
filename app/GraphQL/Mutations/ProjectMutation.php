424b2a17af204e7414d8eb66781d1fd8c5001ad1
Rabin <rabin.awale09@gmail.com>
2025-11-18 13:04:18 +0545

    Update project creation mutation to use authenticated user ID and set API guards

--- app/GraphQL/Mutations/ProjectMutation.php
+++ app/GraphQL/Mutations/ProjectMutation.php
@@ -10,12 +10,12 @@ class ProjectMutation
 {
     public function create($root, array $args, GraphQLContext $context): Project
     {
-        //$user = $context->user();
-        //$user_id = $user->id;
+        $user = $context->user();
+        $user_id = $user->id;
         $project = Project::create([
             'name' => $args['name'],
             'description' => $args['description'],
-            'created_by' => 1,
+            'created_by' => $user_id,
         ]);
 
         return $project;
