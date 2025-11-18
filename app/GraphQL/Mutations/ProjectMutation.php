b21f46405bba5fa651391eef802a284a4d2d368e
Rabin <rabin.awale09@gmail.com>
2025-11-18 08:18:32 +0545

    Add Project model, GraphQL schema, and mutation for project creation

---
+++ app/GraphQL/Mutations/ProjectMutation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+use Nuwave\Lighthouse\Execution\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Models\Project;
+use Auth;
+
+class ProjectMutation
+{
+    public function create($root, array $args, GraphQLContext $context): Project
+    {
+        //$user = $context->user();
+        //$user_id = $user->id;
+        $project = Project::create([
+            'name' => $args['name'],
+            'description' => $args['description'],
+            'created_by' => 6,
+        ]);
+
+        return $project;
+    }
+}
