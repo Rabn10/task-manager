cd514cb5bafcc07b5d97de5b516dc009b92e08ea
Rabin <rabin.awale09@gmail.com>
2025-11-19 20:18:12 +0545

    Add delete_flag to Project model and update GraphQL queries to filter deleted projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['name', 'description', 'created_by'];
+
+    public function scopeDeleted($query) {
+        return $query->where('delete_flag', false);
+    }
 }
